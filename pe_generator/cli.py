ca770d0c773ef3d73735e1d10f01d68597cef8b1
Alberto Eusebi <alberto@albeus.eu>
2019-05-01 06:43:36 +0200

    Essential init cli.

--- pe_generator/cli.py
+++ pe_generator/cli.py
@@ -4,15 +4,5 @@
 import sys
 import click
 
-
-@click.command()
-def main(args=None):
-    """Console script for pe_generator."""
-    click.echo("Replace this message by putting your code into "
-               "pe_generator.cli.main")
-    click.echo("See click documentation at http://click.pocoo.org/")
-    return 0
-
-
 if __name__ == "__main__":
-    sys.exit(main())  # pragma: no cover
+    sys.exit(main())
